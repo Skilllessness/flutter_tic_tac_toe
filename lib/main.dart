f8b8bddad6172a32c4155d4fbee6dd5ba0b77c8e
Srishan Shakya <csunshakya@gmail.com>
2022-03-13 17:20:26 +0545

    initial finished tic tac toe

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,89 +29,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   var tiles = List.filled(9, 0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Sanjiv's App"),
-      ),
-      body: Row(children: [
-        AspectRatio(
-          aspectRatio: 1,
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: [
-              for (var i = 0; i < 9; i++)
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      tiles[i] = 1;
-                      runAi();
-                    });
-                  },
-                  child: Text(
-                    tiles[i] == 0
-                        ? ''
-                        : tiles[i] == 1
-                            ? 'X'
-                            : 'O',
-                  ),
-                ),
-            ],
-          ),
+        title: const Text(
+          "Skilllessness Tic TAc Toe",
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(isWinning(1, tiles)
-                ? "You won!"
-                : isWinning(2, tiles)
-                    ? "You lost!"
-                    : "Your Move"),
-            OutlinedButton(
-              onPressed: () {
-                setState(() {
-                  tiles = List.filled(9, 0);
-                });
-              },
-              child: Text("Restart"),
-            )
-          ],
-        )
-      ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      body: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: [
+                for (var i = 0; i < 9; i++)
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        tiles[i] = 1;
+                        runAi();
+                      });
+                    },
+                    child: Container(
+                      color: tiles[i] == 0
+                          ? Colors.white
+                          : tiles[i] == 1
+                              ? Colors.green
+                              : Colors.yellow,
+                      child: Center(
+                        child: Text(
+                          tiles[i] == 0
+                              ? ''
+                              : tiles[i] == 1
+                                  ? 'X'
+                                  : 'O',
+                          style: const TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(isWinning(1, tiles)
+                  ? "You won!"
+                  : isWinning(2, tiles)
+                      ? "You lost!"
+                      : "Your Move"),
+              OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    tiles = List.filled(9, 0);
+                  });
+                },
+                child: const Text("Restart"),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 
   void runAi() async {
-    await Future.delayed(Duration(milliseconds: 200));
+    await Future.delayed(const Duration(milliseconds: 200));
     int? winning;
     int? blocking;
     int? normal;
@@ -142,12 +111,14 @@ class _MyHomePageState extends State<MyHomePage> {
         continue;
       }
       var future = [...tiles]..[i] = 1;
+      // If we/AI is winning
       if (isWinning(2, future)) {
         winning = i;
       }
 
       future[i] = 1;
 
+      // If Player is winning.
       if (isWinning(1, future)) {
         blocking = i;
       }
@@ -164,13 +135,13 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool isWinning(int who, List<int> future) {
-    return (tiles[0] == who || tiles[1] == who || tiles[2] == who) ||
-        (tiles[3] == who || tiles[4] == who || tiles[5] == who) ||
-        (tiles[6] == who || tiles[7] == who || tiles[8] == who) ||
-        (tiles[0] == who || tiles[4] == who || tiles[8] == who) ||
-        (tiles[2] == who || tiles[4] == who || tiles[6] == who) ||
-        (tiles[0] == who || tiles[3] == who || tiles[6] == who) ||
-        (tiles[1] == who || tiles[4] == who || tiles[7] == who) ||
-        (tiles[2] == who || tiles[5] == who || tiles[8] == who);
+    return (tiles[0] == who && tiles[1] == who && tiles[2] == who) ||
+        (tiles[3] == who && tiles[4] == who && tiles[5] == who) ||
+        (tiles[6] == who && tiles[7] == who && tiles[8] == who) ||
+        (tiles[0] == who && tiles[4] == who && tiles[8] == who) ||
+        (tiles[2] == who && tiles[4] == who && tiles[6] == who) ||
+        (tiles[0] == who && tiles[3] == who && tiles[6] == who) ||
+        (tiles[1] == who && tiles[4] == who && tiles[7] == who) ||
+        (tiles[2] == who && tiles[5] == who && tiles[8] == who);
   }
 }
